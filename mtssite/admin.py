a7971bc563cea6e72f3f21debe7ea99d113cbc31
kerBits <HighBit1337@yandex.ru>
2022-10-31 17:20:27 +0300

    Added custom User model, user creation/update from admin panel

--- mtssite/admin.py
+++ mtssite/admin.py
@@ -1,7 +1,13 @@
+from django import forms
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
 # Register your models here.
-from .models import News
+from .models import News, CustomUser
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
@@ -42,3 +48,65 @@ class NewsAdmin(admin.ModelAdmin):
     date_hierarchy = "publish_date"
     save_on_top = True
   
+
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('personnel_number', 'email', 'fio')
+
+    def clean_password(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(label=("Password"),
+        help_text=("Raw passwords are not stored, so there is no way to see "
+                    "this user's password, but you can change the password "
+                    "using <a href=\"../password/\">this form</a>."))
+
+    class Meta:
+        model = CustomUser
+        fields = ('personnel_number', 'password', 'email', 'fio', 'shop_number', 'is_active', 'is_admin')
+
+
+class UserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ('personnel_number', 'fio', 'shop_number', 'is_active', 'is_admin')
+    list_filter = ('shop_number',)
+    fieldsets = (
+        (None, {'fields': ('personnel_number', 'password')}),
+        ('Personal info', {'fields': ('fio', 'email', 'shop_number')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('personnel_number', 'email', 'fio', 'shop_number', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('personnel_number', 'shop_number', 'fio',)
+    ordering = ('fio',)
+    filter_horizontal = ()
+
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.unregister(Group)
--- mtssite/models.py
+++ mtssite/models.py
@@ -4,10 +4,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.urls import reverse
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager
+)
 
 # Create your models here.
 class Office(models.Model):
-    officename = models.CharField(max_length=255)
+    officename = models.CharField(max_length=255, unique=True)
     officeaddress = models.CharField(max_length=255)
     officehead = models.CharField(max_length=255)
     officeheadtnumber = models.CharField(max_length=255)
@@ -27,4 +30,66 @@ class News(models.Model):
     publish_date = models.DateTimeField(blank=True, null=True)
     published = models.BooleanField(default=False)
 
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field='username')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field='personnel_number')
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, personnel_number, email, fio, password, shop_number=None):
+        if not personnel_number:
+            raise ValueError("Enter personnel number")
+
+        if not email:
+            raise ValueError("Enter email address")
+
+        if not fio:
+            raise ValueError("Enter FIO")
+
+        user = self.model(
+            personnel_number=personnel_number,
+            email=self.normalize_email(email),
+            fio=fio,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, personnel_number, email, fio, password, shop_number=None):
+        user = self.create_user(
+            personnel_number=personnel_number,
+            email=email,
+            fio=fio,
+            password=password,
+            shop_number=shop_number,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    personnel_number = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    fio = models.CharField(max_length=255)
+    shop_number = models.ForeignKey(Office, on_delete=models.CASCADE, to_field='officename', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'personnel_number'
+    REQUIRED_FIELDS = ['email', 'fio',]
+
+    def get_full_name(self):
+        return self.fio
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
